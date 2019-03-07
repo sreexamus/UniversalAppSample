a5c69e49aea11301dcdab2c08741cf08cf4b8c12
sreekanthreddy.iraga <sreekanthreddy.iragamreddy@schwab.com>
2019-03-07 13:38:13 -0600

    Initial Commit

---
+++ UniversalAppSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversalAppSample
+//
+//  Created by Iragam Reddy, Sreekanth Reddy on 3/7/19.
+//  Copyright © 2019 Iragam Reddy, Sreekanth Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
